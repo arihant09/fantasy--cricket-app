fcc8fd1fbf3775a9e413530acd4c9615a9bf0cb6
Arihant Saxena <75349563+arihant09@users.noreply.github.com>
2022-04-28 10:45:07 +0530

    second commit

--- auth/firebase.js
+++ auth/firebase.js
@@ -5,12 +5,12 @@ import * as firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDkeLl6Bb4gvl5KvOBv-IeJ4p1Pz9Ae_m8",
-  authDomain: "fir-auth-34d15.firebaseapp.com",
-  projectId: "fir-auth-34d15",
-  storageBucket: "fir-auth-34d15.appspot.com",
-  messagingSenderId: "889617192224",
-  appId: "1:889617192224:web:299eac18999cde09787345"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
