18042d7ae3345801a5659fcd14960185ca0ddda1
Arihant Saxena <75349563+arihant09@users.noreply.github.com>
2022-05-17 14:06:26 +0530

    Update DrawerNavigator

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
 import CreateScreen from '../screens/CreateScreen';
+import DrawerNavigator from '../navigation/DrawerNavigator';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
@@ -11,7 +12,7 @@ function StackNavigator(props) {
  return (
     <Stack.Navigator>
     <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-    <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
+    <Stack.Screen options={{ headerShown: false }} name="Home" component={DrawerNavigator} />
     <Stack.Screen name="Create Team" component={CreateScreen} />
   </Stack.Navigator>
  );
