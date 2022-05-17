c2b3f7a8c7239d044a44ebc3db863f2ce87f6f1f
Arihant Saxena <75349563+arihant09@users.noreply.github.com>
2022-05-17 14:04:34 +0530

    navigate update

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import DrawerNavigator from './navigation/DrawerNavigator';
+import DrawerNavigator from './navigation/StackNavigator';
 import { Text } from 'react-native';
 import Location from './components/Location';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <DrawerNavigator/>
+      <StackNavigator/>
     </NavigationContainer>
 
   
